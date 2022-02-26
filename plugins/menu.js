330ce0f1286db9456ae9ba0218602ba97bc4ee56
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-02-26 23:44:20 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -268,8 +268,8 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 │⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 ┬
 ├━━━━━━━━━━━━━━━━┈─⋆
-│ ▸ *Author :* Nurutomo
-┴ ▸ *Owner :* Letta
+│ ▸ *Author :* Mr. Bot Lintang
+┴ ▸ *Owner :* Mr. Bot Lintang
 ✧
 ┬ 📌 𝗣𝗶𝗻𝗻𝗲𝗱 :
 │ Tolong jangan dispam ya biar ga delay
