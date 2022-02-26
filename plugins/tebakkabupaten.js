919d06cdd835677d2e7b10add87247f9161a6e42
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-02-26 23:56:48 +0700

    Update tebakkabupaten.js

--- plugins/tebakkabupaten.js
+++ plugins/tebakkabupaten.js
@@ -18,7 +18,7 @@ Ketik ${usedPrefix}tekb untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tebakkabupaten[id] = [
-        await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), caption, '© nekobotz', 'Bantuan', '.teka', m),
+        await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), caption, '© Mr. Bot Lintang', 'Bantuan', '.teka', m),
         json, poin,
         setTimeout(async () => {
             if (conn.tebakkabupaten[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.title}*`, '© nekobotz', 'Tebak Kabupaten', '.tebakkabupaten', conn.tebakkabupaten[id][0])
